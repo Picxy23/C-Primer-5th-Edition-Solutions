2f70ba4da9984c01124a8f4668093e1bc742a0f6
北黒Pictsit <64764533+Picxy23@users.noreply.github.com>
2022-07-21 16:43:45 -0400

    Update Exercise 3.6.cpp

--- Chapter 3/Exercise 3.2.3/Exercise 3.6.cpp
+++ Chapter 3/Exercise 3.2.3/Exercise 3.6.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <string>
+using std::cin; using std::cout;
+using std::endl; using std::string;
 
+int main(){
+    // Get input string
+    string s;
+    getline(cin, s);
+
+    for (auto &c : s)
+        c = 'X'; // Use '' not "". Dealing with char not string.
+
+    cout << s << endl;
+    return 0;
+}
