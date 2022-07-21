8cc5c15cd61cfb7f8f086ae701fbe50e7599de5d
北黒Pictsit <64764533+Picxy23@users.noreply.github.com>
2022-07-21 15:49:48 -0400

    Create Excercise 3.2.cpp

---
+++ Chapter 3/Excercise Section 3.2.2/Excercise 3.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using std::cin; using std::cout;
+using std::endl; using std::string;
+
+int main(){
+    // Line at a time
+    string s;
+
+    // Delete one to run
+    // Reading inputs by line until EOF
+    while (getline(cin, s))
+        return 0;
+
+    // Reading inputs by word until EOF
+    while (cin >> s)
+        return 1;
+}
