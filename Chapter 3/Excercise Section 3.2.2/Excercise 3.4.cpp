35d6d963bc6c6e5bc763567d1d71121ed129691d
北黒Pictsit <64764533+Picxy23@users.noreply.github.com>
2022-07-21 15:30:25 -0400

    Add files via upload

---
+++ Chapter 3/Excercise Section 3.2.2/Excercise 3.4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using std::cin; using std::cout;
+using std::endl; using std::string;
+
+int main(){
+    //Get two strings
+    cout << "Enter two strings:" << endl;
+    string s1;
+    string s2;
+
+    getline(cin, s1);
+    getline(cin, s2);
+
+    cout << s1 << endl << s2 << endl;
+
+    //Compare String
+    if (s1 == s2)
+        cout << "They are equal" << endl;
+    else if (s1 > s2)
+        cout << "First is larger" << endl;
+    else
+        cout << "Second is larger" << endl;
+
+
+    return 0;
+}
