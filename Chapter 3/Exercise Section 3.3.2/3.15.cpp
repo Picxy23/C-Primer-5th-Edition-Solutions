9c0a4fcea40151abc00473616e9a3c89a35e2f47
北黒Pictsit <64764533+Picxy23@users.noreply.github.com>
2022-07-22 16:37:07 -0400

    Create 3.15.cpp

---
+++ Chapter 3/Exercise Section 3.3.2/3.15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin; using std::cout;
+using std::endl; using std::string;
+using std::vector;
+
+int main(){
+    vector<string> v;
+    string s;
+    while (cin >> s)
+        v.push_back(s);
+}
