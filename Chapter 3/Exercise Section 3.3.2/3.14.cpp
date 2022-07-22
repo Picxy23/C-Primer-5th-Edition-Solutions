5a64000519c9ffdc3f6715218d8cde7e9c9d1045
北黒Pictsit <64764533+Picxy23@users.noreply.github.com>
2022-07-22 16:35:56 -0400

    Create 3.14.cpp

---
+++ Chapter 3/Exercise Section 3.3.2/3.14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin; using std::cout;
+using std::endl; using std::string;
+using std::vector;
+
+int main(){
+    vector<int> v;
+    signed int num;
+    while (cin >> num)
+        v.push_back(num);
+}
