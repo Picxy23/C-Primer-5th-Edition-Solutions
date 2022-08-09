0632349ff75d567f1cfd77e954bb3fe09eb7f853
北黒Pictsit <64764533+Picxy23@users.noreply.github.com>
2022-08-09 15:33:58 -0400

    Create 4.4.cpp

---
+++ Chapter 4/Exercise Section 4.2/4.4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <string>
+#include <iterator>
+#include <cstring>
+
+using namespace::std;
+
+int main()
+{
+    cout << 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2 << endl;
+    cout << (((12 / 3) * 4) + (5 * 15) + ((24 % 4) / 2)) << endl;
+    // Result in 91
+    return 0;
+}
