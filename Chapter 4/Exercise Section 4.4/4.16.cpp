2f85b7f7aef6cc355f756fb7d183faa0a0df2270
北黒Pictsit <64764533+Picxy23@users.noreply.github.com>
2022-08-14 23:53:44 -0400

    Create 4.16.cpp

---
+++ Chapter 4/Exercise Section 4.4/4.16.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <string>
+#include <iterator>
+#include <cstring>
+
+using namespace::std;
+
+int main(){
+    if ((p = getPtr()) != 0)
+        cout << "";
+    if (i == 1024)
+        cout << "";
+    return 0;
+}
