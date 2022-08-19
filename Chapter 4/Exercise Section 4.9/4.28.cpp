833ea281a1c998a2dff0bfa64f7845b26ab48d36
北黒Pictsit <64764533+Picxy23@users.noreply.github.com>
2022-08-19 16:18:50 -0400

    Create 4.28.cpp

---
+++ Chapter 4/Exercise Section 4.9/4.28.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <iterator>
+#include <cstring>
+#include <bitset>
+
+using namespace::std;
+
+int main(){
+    cout << sizeof (int) << endl;
+    cout << sizeof (unsigned int) << endl;
+    cout << sizeof (long long int) << endl;
+    cout << sizeof (short int) << endl;
+    // .....
+    return 0;
+}
